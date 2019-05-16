a59241ad435ed6355f411d501c2a9d69a0425be1
wht <859370476@qq.com>
2019-05-16 17:36:04 +0800

    commit

---
+++ gan.py
@@ -0,0 +1,172 @@
+#coding=utf-8
+from __future__ import print_function, division
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+
+import sys
+
+import numpy as np
+
+class GAN():
+    def __init__(self):
+        self.img_rows = 28
+        self.img_cols = 28
+        self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.latent_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes noise as input and generates imgs
+        z = Input(shape=(self.latent_dim,))
+        # 首先先随机生成一张图片
+        img = self.generator(z)
+        # plt.imshow(img)
+        # plt.show()
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated images as input and determines validity
+        validity = self.discriminator(img)
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains the generator to fool the discriminator
+        # 这里的Model是什么？z是生成器的输入，img是生成器的输出，生成器的输出作为判别器的输入，validity为判别器的输出
+        # 这里的Model是keras里面的特定函数，它的作用在于将input和validity纳入到一个容器里面
+        self.combined = Model(z, validity)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer) # 这里的compile可看作是判别器的优化器及损失函数
+
+    # 生成器
+    def build_generator(self):
+        model = Sequential()
+        # 这里的input_dims是什么？它的意思应该是该模型的输入维度为(*, 100) 输出维度(*, 256)
+        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Reshape(self.img_shape))
+        print("生成器的网络模型图")
+        model.summary()
+        # 输入是noise
+        noise = Input(shape=(self.latent_dim,))
+        # 输出是img
+        img = model(noise)
+
+        return Model(noise, img)
+    # 判别器
+    def build_discriminator(self):
+
+        model = Sequential()
+
+        model.add(Flatten(input_shape=self.img_shape))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(256))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+
+        img = Input(shape=self.img_shape)
+        # 这里的意思是将img作为输入，输出得到结果，keras语法，这里的validity是输出结果
+        validity = model(img)
+        # Model(input, output) 这里的Model是啥意思？Model是一个容器，这里img作为输入，vality作为输出，暂时理解为它的意思是将整个模型都纳入到该容器里面
+        return Model(img, validity)
+
+    def train(self, epochs, batch_size=128, sample_interval=50):
+
+        # Load the dataset  这里的dtype类型是float64
+        (X_train, _), (_, _) = mnist.load_data()
+        # Rescale -1 to 1  补充说明: Minst数据集里面，数据大小从0~255 之间波动，所以除以127.5之后，大小在[0, 2]之间
+        X_train = X_train / 127.5 - 1.
+        X_train = np.expand_dims(X_train, axis=3)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random batch of images
+            # 这个两行代码跟shuffle效果一样，从X_train里面随机取一个batch_size数据
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs = X_train[idx]
+
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+
+            # Generate a batch of new images
+            # noise的shape为 [32, 100]
+            gen_imgs = self.generator.predict(noise)
+
+            # Train the discriminator
+            # 这里的imgs因为是从X_train里面取出来的，所以认为其对应的Label都为1，都是真的
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            # 这里的gen_imgs是由生成器生成的
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+
+            # Train the generator (to have the discriminator label samples as valid)
+            g_loss = self.combined.train_on_batch(noise, valid)
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
+
+    def sample_images(self, epoch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
+        gen_imgs = self.generator.predict(noise)
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/%d.png" % epoch)
+        plt.close()
+
+
+if __name__ == '__main__':
+    gan = GAN()
+    gan.train(epochs=30000, batch_size=32, sample_interval=200)
